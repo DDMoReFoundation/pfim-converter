7676535a0e693565c4ff4043357538e9a047863c
grs28b <gareth_r_smith@hotmail.com>
2016-07-12 09:45:43 +0100

    Updated IP declaration

--- converter-pfim/src/main/java/inserm/converters/pfim/scriptlets/FactoryImpl.java
+++ converter-pfim/src/main/java/inserm/converters/pfim/scriptlets/FactoryImpl.java
@@ -1,6 +1,18 @@
-/*******************************************************************************
- * Copyright (C) 2015 Cyprotex Discovery Ltd - All rights reserved.
- ******************************************************************************/
+/* ----------------------------------------------------------------------------
+ * This file is part of PharmML to PFIM 4.0 Converter.  
+ * 
+ * Copyright (C) 2016 jointly by the following organisations:- 
+ * 1. INSERM, Paris, France
+ * 2. Universite Paris Diderot, Paris, France
+ * 3. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ * 4. Cyprotex Discovery Ltd, Macclesfield, England, UK
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation. A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution.
+ * ----------------------------------------------------------------------------
+ */
 
 package inserm.converters.pfim.scriptlets;
 
